25329b1dc1ad00d13bce37e456db6a526b0702e5
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-06-09 20:58:19 +0200

    adds corrections according to recommendations

--- src/components/FeedBack/Statistics.jsx
+++ src/components/FeedBack/Statistics.jsx
@@ -1,30 +1,25 @@
 import PropTypes from 'prop-types';
-import Notification from './Notification';
 
 function Statistics({ good, neutral, bad, total, positivePercentage }) {
-  if (!total) {
-    return <Notification message="There is no feedback" />;
-  } else {
-    return (
-      <ul>
-        <li>
-          <p>Good: {good}</p>
-        </li>
-        <li>
-          <p>Neutral: {neutral}</p>
-        </li>
-        <li>
-          <p>Bad: {bad}</p>
-        </li>
-        <li>
-          <p>Total: {total}</p>
-        </li>
-        <li>
-          <p>Positive feedback: {positivePercentage || 0}%</p>
-        </li>
-      </ul>
-    );
-  }
+  return (
+    <ul>
+      <li>
+        <p>Good: {good}</p>
+      </li>
+      <li>
+        <p>Neutral: {neutral}</p>
+      </li>
+      <li>
+        <p>Bad: {bad}</p>
+      </li>
+      <li>
+        <p>Total: {total}</p>
+      </li>
+      <li>
+        <p>Positive feedback: {positivePercentage || 0}%</p>
+      </li>
+    </ul>
+  );
 }
 
 Statistics.propTypes = {
